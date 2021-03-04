923a0db98de5047ef0542491ef1982cf14cedc18
Kyle <florez.photo@gmail.com>
2021-03-03 19:15:09 -0600

    wrote out a small game plan for a mvp and what i need to code i guess

---
+++ questions.js
@@ -0,0 +1,13 @@
+
+
+// object array that holds -question - choices - answer  --- I guess I just gotta pull basic coding questions from somewhere online??
+
+
+// need score counter
+
+
+//need timer
+//timer countdown
+// 10 second penalizer for getting a wrong answer
+// allowed time per question???
+

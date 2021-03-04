46d80086372b671c27d4c5e707e1312951640d2a
Kyle <florez.photo@gmail.com>
2021-03-03 19:34:55 -0600

    made array that holds questions  , choices, and asnwers that will be cycled through

--- questions.js
+++ questions.js
@@ -1,7 +1,49 @@
+// object array that holds -question - choices - answer  --- I guess I just gotta pull basic coding questions from somewhere online?? how many q's?  5? 6? 10000000???
 
+// questions - answers taken from https://www.tutorialspoint.com/javascript/javascript_online_quiz.htm
+let questions = [
+    {
+        prompt: 'Which built-in method removes the last element from an array and returns that element?',
 
-// object array that holds -question - choices - answer  --- I guess I just gotta pull basic coding questions from somewhere online??
+        choices: ['last()', 'get()', 'pop()', 'None of the above'],
 
+        answer: 'pop()'
+
+    },
+    {
+        prompt: 'Which of the following function of String object returns the characters in a string beginning at the specified location through the specified number of characters?',
+
+        choices: ['slice()', 'split()', 'substr()', 'search()'],
+
+        answer: 'substr()'
+
+    },
+
+    {
+        prompt: 'Which of the following function of Array object creates a new array with all of the elements of this array for which the provided filtering function returns true?',
+
+        choices: ['concat()', 'every()', 'filter()', 'some()'],
+
+        answer: 'filter()'
+
+    },
+    {
+        prompt: 'Which of the following function of Array object adds one or more elements to the front of an array and returns the new length of the array?',
+
+        choices: ['unshift()', 'sort()', 'splice()', 'toString()'],
+
+        answer: 'unshift()'
+
+    },
+    {
+        prompt: 'What is the HTML tag under which one can write the JavaScript code?',
+
+        choices: ['<javascript>', '<scripted>', '<script>', '<js>'],
+
+        answer: '<script>'
+    },
+
+];
 
 // need score counter
 
@@ -11,3 +53,4 @@
 // 10 second penalizer for getting a wrong answer
 // allowed time per question???
 
+// prompt when wrong answer chosen
